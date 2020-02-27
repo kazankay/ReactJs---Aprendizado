c6fef91072151775786d497f07e23a021734a031
Matheus <matheusccontato@gmail.com>
2020-02-26 23:06:35 -0300

    Primeiro Projeto ReactJs

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import Routes from './routes'
+
+import Header from './components/Header';
+import Main from './pages/main';
+
+import "./styles.css"
+
+const App = () => {
+  return(
+    <div id="ClassName">
+      <Header />
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
